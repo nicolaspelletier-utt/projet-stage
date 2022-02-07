f5a816c9971b857cb137d6ec47ef8bfd3e5d47d0
Nicolas Pelletier <nicolas@grytics.com>
2022-02-07 15:24:15 +0100

    fix sql query

--- src/Controller/StatsController.php
+++ src/Controller/StatsController.php
@@ -42,10 +42,10 @@ class StatsController extends AbstractController {
         $session->start();
         if (!empty($session->get('logged'))) {
             $db=$this->model->getInstance();
-            if ($request->query->has('beggin') && $request->query->has('end')) {
+            if ($request->query->has('begin') && $request->query->has('end')) {
                 $begin=$request->query->get('begin');
                 $end=$request->query->get('end');
-                $query= 'select avg(de.value) from (select count(c.comment_id) as value from data_comments where created_time between ? and ? c group by c.post_id) de  ';
+                $query= 'select avg(de.value) from (select count(c.comment_id) as value from data_comments c where c.created_time between ? and ? group by c.post_id) de  ';
                 $statement=$db->prepare($query);
                 $statement->execute(array(htmlspecialchars($begin),htmlspecialchars($end)));
             }
@@ -72,7 +72,7 @@ class StatsController extends AbstractController {
         $session->start();
         if (!empty($session->get('logged'))) {
             $db=$this->model->getInstance();
-            if ($request->query->has('beggin') && $request->query->has('end')) {
+            if ($request->query->has('begin') && $request->query->has('end')) {
                 $begin=$request->query->get('begin');
                 $end=$request->query->get('end');
                 $query='select u.people_name, count(p.post_id) from data_people u, data_posts p where u.people_id = p.people_id and p.created_time between ? and ? group by u.people_id order by  count(p.post_id) DESC limit 10 ';
@@ -102,7 +102,7 @@ class StatsController extends AbstractController {
         $session->start();
         if (!empty($session->get('logged'))) {
             $db=$this->model->getInstance();
-            if ($request->query->has('beggin') && $request->query->has('end')) {
+            if ($request->query->has('begin') && $request->query->has('end')) {
                 $begin=$request->query->get('begin');
                 $end=$request->query->get('end');
                 $query="select distinct dp.people_name from data_people dp where dp.people_id not in (select r.people_id from data_reactions r where r.created_time between '2021-07-01 15:02:00' and '2021-07-31 15:02:30') and dp.people_id not in (select c.people_id from data_comments c where c.created_time between ? and ?) limit 10";
